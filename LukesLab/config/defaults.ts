1111188f1932c09ec79982b251c040f3a11e935c
Juan-LukeKlopper <luke@stakeordie.com>
2023-01-19 22:34:22 +0200

    feat: Update Chain info in configs

--- LukesLab/config/defaults.ts
+++ LukesLab/config/defaults.ts
@@ -1,1 +1,16 @@
-export const chainName = 'cosmoshub';
+import { assets } from 'chain-registry';
+import { AssetList, Asset } from '@chain-registry/types';
+
+export const chainName = 'cosmwasmtestnet';
+export const stakingDenom = 'umlg';
+export const feeDenom = 'uand';
+
+export const cw20ContractAddress = 'wasm1lr7rngr9x3ssgle3xv0rgtndzz05e92p6q0zlvd0w8pm8al08r0sw90t9u'
+
+export const chainassets: AssetList = assets.find(
+    (chain) => chain.chain_name === chainName
+) as AssetList;
+
+export const coin: Asset = chainassets.assets.find(
+    (asset) => asset.base === stakingDenom
+) as Asset;
